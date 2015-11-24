34fc7236fb682e93283246c773018831a94a085a
Mohamed Nimir <mohamed.nimir@eptikar.com>
2015-11-24 09:41:46 +0300

    added missing layout helpers

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -16,5 +16,20 @@ module LayoutHelper
   def javascript(*args)
     content_for(:head) { javascript_include_tag(*args) }
   end
+  
+  def bootstrap_class_for flash_type
+    case flash_type
+      when :success
+        "alert-success" # Green
+      when :danger
+        "alert-danger" # Red
+      when :alert
+        "alert-warning" # Yellow
+      when :notice
+        "alert-info" # Blue
+      else
+        flash_type.to_s
+    end
+  end
 
 end
