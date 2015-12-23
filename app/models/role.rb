9913694a245ca270dd24ad9b670bbd8feedcf0cb
Arwa Ahmed <arwa.tayeb@eptikar.com>
2015-12-23 11:33:28 +0300

    showing checked elements in edit views

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,4 +4,15 @@ class Role < ActiveRecord::Base
   has_many :project_roles
 
   serialize :permissions, Hash
+  
+  def search_hash(h, search)
+    return h[search] if h.fetch(search, false)
+
+    h.keys.each do |k|
+     answer = search_hash(h[k], search) if h[k].is_a? Hash
+     return answer if answer
+    end
+
+    false
+  end
 end
