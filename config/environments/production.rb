a673cd235144e44d142abff0e24938a6fda444fa
Arwa Ahmed <arwa.tayeb@eptikar.com>
2015-12-29 14:05:22 +0300

    changing production action mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'open-ngo.heroku.com' }
+  config.action_mailer.default_url_options = { host: 'open-ngo.herokuapp.com' }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
