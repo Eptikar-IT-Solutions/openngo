14010bb04da25c4a246e87a8c32442d5c00eb48a
Arwa Ahmed <arwa.tayeb@eptikar.com>
2015-12-30 12:49:01 +0300

    added settings menu

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,6 @@ class SessionsController < Devise::SessionsController
   def new
     self.resource = resource_class.new(sign_in_params)
     clean_up_passwords(resource)
-    render :layout => false
+    render layout: false
   end
 end
