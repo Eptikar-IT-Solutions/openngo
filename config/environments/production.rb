ed58b5d0586b9b7c825eb1cc3f2d09f1f6d7224c
Arwa Ahmed <arwa.tayeb@eptikar.com>
2016-01-05 15:24:27 +0300

    change smtp production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,14 +9,20 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
+  ##
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { :address => "mailer.eptikar.com" }
+  ##
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'open-ngo.herokuapp.com' }
-
+  config.action_mailer.smtp_settings = { :address => "mailer.eptikar.com" }
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
