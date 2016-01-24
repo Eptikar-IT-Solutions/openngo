09cd3ef4e7737911a5b0bf697a7d54417a5457bc
Arwa Ahmed <arwa.tayeb@eptikar.com>
2016-01-24 15:47:09 +0300

    added trello-webhook config removed arwa method

--- app/controllers/trello_webhooks_controller.rb
+++ app/controllers/trello_webhooks_controller.rb
@@ -1,13 +1,6 @@
 class TrelloWebhooksController < ActionController::Base
   include TrelloWebhook::Processor
 
-  def update_board
-    puts params 
-    # project = Project.find_by(trello_board_id: board.id)
-    # project.update(name: board.name, description: board.desc)
-    render :nothing => true
-  end
-
   def webhook_secret
     ENV['TRELLO_KEY']
   end
