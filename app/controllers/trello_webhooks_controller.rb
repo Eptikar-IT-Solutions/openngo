b97966943caaf42713e15c291dd4f10213a936ab
Arwa Ahmed <arwa.tayeb@eptikar.com>
2016-01-24 22:56:14 +0300

    modified webhoks controller: puts action data

--- app/controllers/trello_webhooks_controller.rb
+++ app/controllers/trello_webhooks_controller.rb
@@ -4,6 +4,10 @@ class TrelloWebhooksController < ActionController::Base
   def webhook_secret
     puts '##########################################OVER HERE############'
     puts event
+    puts '####################333board#########'
+    puts json_body["action"]["data"]["board"]
+    puts '####################card#########'
+    puts json_body["action"]["data"]["card"]
     puts '##########################################OVER HERE############'
     ENV['TRELLO_KEY']
   end
