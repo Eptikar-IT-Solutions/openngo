35e6a52dc2976fe4be4e1c77d87f05ee27efb883
Arwa Ahmed <arwa.tayeb@eptikar.com>
2016-01-24 15:14:03 +0300

    modified trello_webhooks

--- app/controllers/trello_webhooks_controller.rb
+++ app/controllers/trello_webhooks_controller.rb
@@ -2,11 +2,9 @@ class TrelloWebhooksController < ActionController::Base
   include TrelloWebhook::Processor
 
   def update_board
-    payload = 
-    #board = payload[1]["action"]["model"]
-    board
-    project = Project.find_by(trello_board_id: board.id)
-    project.update(name: board.name, description: board.desc)
+    puts params 
+    # project = Project.find_by(trello_board_id: board.id)
+    # project.update(name: board.name, description: board.desc)
     render :nothing => true
   end
 end
