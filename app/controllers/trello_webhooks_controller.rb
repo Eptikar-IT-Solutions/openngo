25de90a07d5b9e2a07404ef02530e5efb102230c
Arwa Ahmed <arwa.tayeb@eptikar.com>
2016-01-25 10:03:01 +0300

    the trello webhook controller: puts json_body,board

--- app/controllers/trello_webhooks_controller.rb
+++ app/controllers/trello_webhooks_controller.rb
@@ -4,14 +4,16 @@ class TrelloWebhooksController < ActionController::Base
   def webhook_secret
     puts '##########################################OVER HERE############'
     puts event
-    puts json_body
+    puts '^^^^^^^^^^^^^^^^^^^^json_body^^^^^^^^^^#{json_body}'
+    board =  json_body["action"]["data"]["board"]
     puts '####################333board#########'
-    puts json_body["action"]["data"]["board"]
+    puts '^^^^^^^^^^^^^^^^^^^^json_body^^^^^^^^^^#{board}'
+    card =  json_body["action"]["data"]["card"]
     puts '####################card#########'
-    puts json_body["action"]["data"]["card"]
+    puts '^^^^^^^^^^^^^^^^^^^^json_body^^^^^^^^^^#{card}'
     puts '##########################################OVER HERE############'
     if event == "create_card"
-      Activity.create(name: json_body["action"]["data"]["card"]["name"])
+      Activity.create(name: json_body["action"]["data"]["card"]["name"], project_id: Project.find_by(board_id: json_body["model"]["id"]))
     elsif event == "update_board"
      Project.find_by(board_id: json_body["model"]["id"]).update_attributes(name: json_body["action"]["data"]["board"]["name"], description: json_body["model"]["desc"]) 
     # elsif event = "update_card"
