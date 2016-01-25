a4faf30ef9be60a689cbec4d5d09df1cf3768d86
Arwa Ahmed <arwa.tayeb@eptikar.com>
2016-01-25 13:29:01 +0300

    debugging board_webhook

--- app/controllers/trello_webhooks_controller.rb
+++ app/controllers/trello_webhooks_controller.rb
@@ -1,7 +1,10 @@
 class TrelloWebhooksController < ActionController::Base
   include TrelloWebhook::Processor
-  
+
   def board_webhooks
+    puts '################################################3'
+    puts json_body
+    puts '################################################3'
     case json_body["action"]["data"]["old"].keys.first
       when "desc"
         Project.find_by(board: json_body["model"]["id"]).update_attributes(description: json_body["action"]["data"]["board"]["desc"]) 
