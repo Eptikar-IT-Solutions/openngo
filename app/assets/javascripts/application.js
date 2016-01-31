f4cd89e8b1e583da2dd9d94e83ce0a899f79e9bb
Arwa Ahmed <arwa.tayeb@eptikar.com>
2016-01-31 14:54:22 +0300

    removed tagging functions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -79,38 +79,38 @@ $( document ).ready(function() {
 	  clean: false
 	});
 
-  tags = [];
+ //  tags = [];
 
-  try { $('input.tag-input-field').tagsinput(); $('.bootstrap-tagsinput input[size=8]').typeahead({source: $('.tag-input-field').data('source')});}
-  catch(err) { }
+ //  try { $('input.tag-input-field').tagsinput(); $('.bootstrap-tagsinput input[size=8]').typeahead({source: $('.tag-input-field').data('source')});}
+ //  catch(err) { }
 
-  $('.edit-tags').click(editTags);
+ //  $('.edit-tags').click(editTags);
 
-  $('#entities').click(function(){ $("#search-form").slideUp( "slow", function() {}); })
+ //  $('#entities').click(function(){ $("#search-form").slideUp( "slow", function() {}); })
 
-  $('.tag-link').mousedown(function(e){
-    document.oncontextmenu = function() {return false;};
-    if( e.button == 2 ) {
-      editTag(this);
-      return false;
-    } else {
-      var tag = $(this).text();
-      if ( tags.indexOf(tag) > -1 ) tags.splice(tags.indexOf(tag), 1);
-      else tags.push(tag);
-      updateTags();
-      return false;
-    }
-  });
+ //  $('.tag-link').mousedown(function(e){
+ //    document.oncontextmenu = function() {return false;};
+ //    if( e.button == 2 ) {
+ //      editTag(this);
+ //      return false;
+ //    } else {
+ //      var tag = $(this).text();
+ //      if ( tags.indexOf(tag) > -1 ) tags.splice(tags.indexOf(tag), 1);
+ //      else tags.push(tag);
+ //      updateTags();
+ //      return false;
+ //    }
+ //  });
 
-	$("table.sort").tablesorter( {sortList: [[0,0]]} );
+	// $("table.sort").tablesorter( {sortList: [[0,0]]} );
 
-  $('.tag-link').click(function(){return false;});
+ //  $('.tag-link').click(function(){return false;});
 
-  $('textarea').autosize();
+ //  $('textarea').autosize();
 
-  $('.autocomplete').typeahead({source: $('.autocomplete').data('source')});
+ //  $('.autocomplete').typeahead({source: $('.autocomplete').data('source')});
 
-  $('.steps-color-change').click(function() { changeStepsColor(this); return false; })
+ //  $('.steps-color-change').click(function() { changeStepsColor(this); return false; })
 
 });
 
@@ -132,218 +132,218 @@ function resizeMenu() {
   $('.page-header').css('width', $('.row').first().width() - 30);
 }
 
-var tags = [];
+// var tags = [];
 
-function editTags(){
-  var path = $(this).data('path');
-  var tags = $(this).data('tags');
-  var model = $(this).data('model');
+// function editTags(){
+//   var path = $(this).data('path');
+//   var tags = $(this).data('tags');
+//   var model = $(this).data('model');
 
-  var modal = '<div id="tags-modal" class="modal fade">\
-    <div class="modal-dialog">\
-      <div class="modal-content">\
-        <div class="modal-header">\
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button\
-          <h4 class="modal-title">Edit Tags</h4>\
-        </div>\
-        <div class="modal-body">\
-          <div class="form-group">\
-            <label class="col-sm-2 control-label" for="target_Tags">Tags</label>\
-            <div class="col-sm-10"><input type="text" value="'+ tags +'" data-role="tagsinput" placeholder="Add tags" style="display: none;" class="form-control tag-input-field"></div>\
-          </div>\
-        </div>\
-        <div class="modal-footer">\
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
-          <button type="button" class="btn btn-primary">Save changes</button>\
-        </div>\
-      </div><!-- /.modal-content -->\
-    </div><!-- /.modal-dialog -->\
-  </div><!-- /.modal -->';
+//   var modal = '<div id="tags-modal" class="modal fade">\
+//     <div class="modal-dialog">\
+//       <div class="modal-content">\
+//         <div class="modal-header">\
+//           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button\
+//           <h4 class="modal-title">Edit Tags</h4>\
+//         </div>\
+//         <div class="modal-body">\
+//           <div class="form-group">\
+//             <label class="col-sm-2 control-label" for="target_Tags">Tags</label>\
+//             <div class="col-sm-10"><input type="text" value="'+ tags +'" data-role="tagsinput" placeholder="Add tags" style="display: none;" class="form-control tag-input-field"></div>\
+//           </div>\
+//         </div>\
+//         <div class="modal-footer">\
+//           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+//           <button type="button" class="btn btn-primary">Save changes</button>\
+//         </div>\
+//       </div><!-- /.modal-content -->\
+//     </div><!-- /.modal-dialog -->\
+//   </div><!-- /.modal -->';
 
-  $('body').append(modal);
-  $('#tags-modal').modal();
+//   $('body').append(modal);
+//   $('#tags-modal').modal();
 
-  $('#tags-modal').on('hidden.bs.modal', function () {
-      $(this).data('bs.modal', null);
-      $('#tags-modal').remove();
-  });
+//   $('#tags-modal').on('hidden.bs.modal', function () {
+//       $(this).data('bs.modal', null);
+//       $('#tags-modal').remove();
+//   });
 
-  $('#tags-modal input.tag-input-field').tagsinput();
+//   $('#tags-modal input.tag-input-field').tagsinput();
 
-  $('#tags-modal').on('shown.bs.modal', function () {
-    $('#tags-modal input.tag-input-field').focus();
-    $('.bootstrap-tagsinput input[size=8]').autocomplete({source: $('.tags-cloud').data('source')});
-  });
+//   $('#tags-modal').on('shown.bs.modal', function () {
+//     $('#tags-modal input.tag-input-field').focus();
+//     $('.bootstrap-tagsinput input[size=8]').autocomplete({source: $('.tags-cloud').data('source')});
+//   });
 
-  $('#tags-modal .btn-primary').click(function(){
-    $('#tags-modal').modal('hide');
+//   $('#tags-modal .btn-primary').click(function(){
+//     $('#tags-modal').modal('hide');
 
-    var data = {};
-    data[model] = {tag_names: $('#tags-modal .tag-input-field').val()};
-    data['_method'] = 'put';
+//     var data = {};
+//     data[model] = {tag_names: $('#tags-modal .tag-input-field').val()};
+//     data['_method'] = 'put';
 
-    $.ajax({ type: "PUT", dataType: "JSON", url: path, contentType: 'application/json', data: JSON.stringify(data) }).done(function( msg ) {
-      $('#pjax-container').load( window.location.pathname + '?_pjax=1', function() {
-        ready();
-        $('#title').remove();
-      });
-    });
+//     $.ajax({ type: "PUT", dataType: "JSON", url: path, contentType: 'application/json', data: JSON.stringify(data) }).done(function( msg ) {
+//       $('#pjax-container').load( window.location.pathname + '?_pjax=1', function() {
+//         ready();
+//         $('#title').remove();
+//       });
+//     });
 
-  });
-}
+//   });
+// }
 
-function editTag(tag_link){
-  var tag = $(tag_link).text();
-  var path = $(tag_link).data('path');
-  var color_code = $(tag_link).data('color');
+// function editTag(tag_link){
+//   var tag = $(tag_link).text();
+//   var path = $(tag_link).data('path');
+//   var color_code = $(tag_link).data('color');
 
-  var modal = '<div id="tags-modal" class="modal fade">\
-    <div class="modal-dialog">\
-      <div class="modal-content">\
-        <div class="modal-header">\
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button\
-          <h4 class="modal-title">Edit '+ tag +'</h4>\
-        </div>\
-        <div class="modal-body">\
-          <div class="form-group">\
-            <label class="col-sm-2 control-label" for="target_Tags">Color</label>\
-            <div class="col-sm-10">\
-              <input id="color_code" type="text" class="form-control colorpicker" style="border:none;color:#FFF;background-color:'+ color_code +'" >\
-            </div>\
-          </div>\
-        </div>\
-        <div class="modal-footer">\
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
-          <button type="button" class="btn btn-primary">Save changes</button>\
-        </div>\
-      </div><!-- /.modal-content -->\
-    </div><!-- /.modal-dialog -->\
-  </div><!-- /.modal -->';
+//   var modal = '<div id="tags-modal" class="modal fade">\
+//     <div class="modal-dialog">\
+//       <div class="modal-content">\
+//         <div class="modal-header">\
+//           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button\
+//           <h4 class="modal-title">Edit '+ tag +'</h4>\
+//         </div>\
+//         <div class="modal-body">\
+//           <div class="form-group">\
+//             <label class="col-sm-2 control-label" for="target_Tags">Color</label>\
+//             <div class="col-sm-10">\
+//               <input id="color_code" type="text" class="form-control colorpicker" style="border:none;color:#FFF;background-color:'+ color_code +'" >\
+//             </div>\
+//           </div>\
+//         </div>\
+//         <div class="modal-footer">\
+//           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+//           <button type="button" class="btn btn-primary">Save changes</button>\
+//         </div>\
+//       </div><!-- /.modal-content -->\
+//     </div><!-- /.modal-dialog -->\
+//   </div><!-- /.modal -->';
 
-  $('body').append(modal);
-  $('#tags-modal').modal();
+//   $('body').append(modal);
+//   $('#tags-modal').modal();
 
-  $('#tags-modal').on('hidden.bs.modal', function () {
-      $(this).data('bs.modal', null);
-      $('#tags-modal').remove();
-      $('.colorpicker.dropdown-menu').remove();
-      document.oncontextmenu = function() {return true;};
-  });
+//   $('#tags-modal').on('hidden.bs.modal', function () {
+//       $(this).data('bs.modal', null);
+//       $('#tags-modal').remove();
+//       $('.colorpicker.dropdown-menu').remove();
+//       document.oncontextmenu = function() {return true;};
+//   });
 
-  $('#color_code').val(color_code);
+//   $('#color_code').val(color_code);
 
-  $('#tags-modal').on('shown.bs.modal', function () {
-    $('.colorpicker').focus();
-  });
+//   $('#tags-modal').on('shown.bs.modal', function () {
+//     $('.colorpicker').focus();
+//   });
 
-  $('.colorpicker').colorpicker().on('changeColor', function(ev){
-    $('.colorpicker').css('background-color', ev.color.toHex());
-  });
+//   $('.colorpicker').colorpicker().on('changeColor', function(ev){
+//     $('.colorpicker').css('background-color', ev.color.toHex());
+//   });
 
-  $('#tags-modal .btn-primary').click(function(){
-    $('#tags-modal').modal('hide');
+//   $('#tags-modal .btn-primary').click(function(){
+//     $('#tags-modal').modal('hide');
 
-    var data = { tag:{ color_code: $('#color_code').val() }, _method: 'put' };
+//     var data = { tag:{ color_code: $('#color_code').val() }, _method: 'put' };
 
-    $.ajax({ type: "PUT", dataType: "JSON", url: path, contentType: 'application/json', data: JSON.stringify(data) }).done(function( msg ) {
-      $('#pjax-container').load( window.location.pathname + '?_pjax=1', function() {
-        ready();
-        $('#title').remove();
-      });
-    });
+//     $.ajax({ type: "PUT", dataType: "JSON", url: path, contentType: 'application/json', data: JSON.stringify(data) }).done(function( msg ) {
+//       $('#pjax-container').load( window.location.pathname + '?_pjax=1', function() {
+//         ready();
+//         $('#title').remove();
+//       });
+//     });
 
-  });
-}
+//   });
+// }
 
-function changeStepsColor(action_link){
-  var path = $(action_link).data('path');
-  var steps_array = $('input[name=selected_steps]:checked').map(function() { return this.value; });
+// function changeStepsColor(action_link){
+//   var path = $(action_link).data('path');
+//   var steps_array = $('input[name=selected_steps]:checked').map(function() { return this.value; });
 
-  var modal = '<div id="steps-coloring-modal" class="modal fade">\
-    <div class="modal-dialog">\
-      <div class="modal-content">\
-        <div class="modal-body">\
-          <div class="form-group">\
-            <label class="col-sm-2 control-label" for="target_Tags">Color</label>\
-            <div class="col-sm-10">\
-              <input id="color_code" type="text" class="form-control colorpicker" style="border:none;color:#FFF;" >\
-            </div>\
-          </div>\
-        </div>\
-        <div class="modal-footer">\
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
-          <button type="button" class="btn btn-primary">Save changes</button>\
-        </div>\
-      </div><!-- /.modal-content -->\
-    </div><!-- /.modal-dialog -->\
-  </div><!-- /.modal -->';
+//   var modal = '<div id="steps-coloring-modal" class="modal fade">\
+//     <div class="modal-dialog">\
+//       <div class="modal-content">\
+//         <div class="modal-body">\
+//           <div class="form-group">\
+//             <label class="col-sm-2 control-label" for="target_Tags">Color</label>\
+//             <div class="col-sm-10">\
+//               <input id="color_code" type="text" class="form-control colorpicker" style="border:none;color:#FFF;" >\
+//             </div>\
+//           </div>\
+//         </div>\
+//         <div class="modal-footer">\
+//           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+//           <button type="button" class="btn btn-primary">Save changes</button>\
+//         </div>\
+//       </div><!-- /.modal-content -->\
+//     </div><!-- /.modal-dialog -->\
+//   </div><!-- /.modal -->';
 
-  $('body').append(modal);
-  $('#steps-coloring-modal').modal();
+//   $('body').append(modal);
+//   $('#steps-coloring-modal').modal();
 
-  $('#steps-coloring-modal').on('hidden.bs.modal', function () {
-      $(this).data('bs.modal', null);
-      $('#steps-coloring-modal').remove();
-      $('.colorpicker.dropdown-menu').remove();
-      document.oncontextmenu = function() {return true;};
-  });
+//   $('#steps-coloring-modal').on('hidden.bs.modal', function () {
+//       $(this).data('bs.modal', null);
+//       $('#steps-coloring-modal').remove();
+//       $('.colorpicker.dropdown-menu').remove();
+//       document.oncontextmenu = function() {return true;};
+//   });
 
-  $('#steps-coloring-modal').on('shown.bs.modal', function () {
-    $('.colorpicker').focus();
-  });
+//   $('#steps-coloring-modal').on('shown.bs.modal', function () {
+//     $('.colorpicker').focus();
+//   });
 
-  $('.colorpicker').colorpicker().on('changeColor', function(ev){
-    $('.colorpicker').css('background-color', ev.color.toHex());
-  });
+//   $('.colorpicker').colorpicker().on('changeColor', function(ev){
+//     $('.colorpicker').css('background-color', ev.color.toHex());
+//   });
 
-  $('#steps-coloring-modal .btn-primary').click(function(){
-    $('#steps-coloring-modal').modal('hide');
+//   $('#steps-coloring-modal .btn-primary').click(function(){
+//     $('#steps-coloring-modal').modal('hide');
 
-    var data = { steps_ids: steps_array.get(), color_code: $('#color_code').val(), _method: 'post'};
+//     var data = { steps_ids: steps_array.get(), color_code: $('#color_code').val(), _method: 'post'};
 
-    $.ajax({ type: "POST", dataType: "JSON", url: path, contentType: 'application/json', data: JSON.stringify(data) }).done(function( msg ) {
-      $('#pjax-container').load( window.location.pathname + '?_pjax=1', function() {
-        ready();
-        $('#title').remove();
-      });
-    });
+//     $.ajax({ type: "POST", dataType: "JSON", url: path, contentType: 'application/json', data: JSON.stringify(data) }).done(function( msg ) {
+//       $('#pjax-container').load( window.location.pathname + '?_pjax=1', function() {
+//         ready();
+//         $('#title').remove();
+//       });
+//     });
 
-  });
-}
+//   });
+// }
 
-function updateTags() {
+// function updateTags() {
 
-  if(tags.length > 0) {
-    $('.entity-box').hide();
-    $('table tbody tr').hide();
-  }
-  else {
-    $('.entity-box').show();
-    $('table tbody tr').show();
-  }
-  $('.tags-cloud .tags li a.active').removeClass('active');
+//   if(tags.length > 0) {
+//     $('.entity-box').hide();
+//     $('table tbody tr').hide();
+//   }
+//   else {
+//     $('.entity-box').show();
+//     $('table tbody tr').show();
+//   }
+//   $('.tags-cloud .tags li a.active').removeClass('active');
 
-  $(tags).each(function(){
-    tag = this;
-    $('.tags-cloud .tags li a:contains("'+ tag +'")').addClass('active');
+//   $(tags).each(function(){
+//     tag = this;
+//     $('.tags-cloud .tags li a:contains("'+ tag +'")').addClass('active');
 
-    $('.entity-box').each(function() {
-      var entity = this;
-      $(this).find('.entity-tags .tags li a').each(function() {
-        if ($(this).text().toLowerCase().indexOf(tag.trim().toLowerCase()) >= 0) $(entity).show();
-      });
-    });
+//     $('.entity-box').each(function() {
+//       var entity = this;
+//       $(this).find('.entity-tags .tags li a').each(function() {
+//         if ($(this).text().toLowerCase().indexOf(tag.trim().toLowerCase()) >= 0) $(entity).show();
+//       });
+//     });
 
-    $('table tbody tr').each(function() {
-      var entity = this;
-      $(this).find('.tags li a').each(function() {
-        if ($(this).text().toLowerCase().indexOf(tag.trim().toLowerCase()) >= 0) $(entity).show();
-      });
-    });
+//     $('table tbody tr').each(function() {
+//       var entity = this;
+//       $(this).find('.tags li a').each(function() {
+//         if ($(this).text().toLowerCase().indexOf(tag.trim().toLowerCase()) >= 0) $(entity).show();
+//       });
+//     });
 
-  });
+//   });
 
-  updateGroups();
+//   updateGroups();
 
-  return false;
-}
+//   return false;
+// }
