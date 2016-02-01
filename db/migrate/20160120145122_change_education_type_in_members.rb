66a340a51f2d02afb86c942546d9ef2d2205ab88
Mohammed Muntaser <mohamed.muntasir@eptikar.com>
2016-02-01 13:38:26 +0300

    changed migration to accomodate pgsql

--- db/migrate/20160120145122_change_education_type_in_members.rb
+++ db/migrate/20160120145122_change_education_type_in_members.rb
@@ -1,7 +1,7 @@
 class ChangeEducationTypeInMembers < ActiveRecord::Migration
   def change
     if connection.adapter_name.downcase == 'postgresql'
-        change_column :members, :education, 'integer USING CAST(`education` AS integer)'
+        change_column :members, :education, 'integer USING CAST(education AS integer)'
       else
         change_column :members, :education, :integer
       end
