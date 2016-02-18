203e03b1a109d870120850e461d048e8810fb939
Arwa Ahmed <arwa.tayeb@eptikar.com>
2016-02-18 17:34:17 +0300

    cleaned up test

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Member, type: :model do
   it { should validate_presence_of :name }
-  it { should validate_presence_of :email}
   it { should validate_presence_of :mobile }
   it { should validate_uniqueness_of  :name }
 
