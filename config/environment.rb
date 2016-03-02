b9f366e039cfe608d09a4347f10a90b743ba200d
Arwa Ahmed <arwa.tayeb@eptikar.com>
2016-03-02 11:13:20 +0300

    minor configuration cchanges

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+config.action_mailer.default_url_options = { host: 'openngo-demo.herokuapp.com' }
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'openngo.eptikar.com' }
+  config.action_mailer.default_url_options = { host: 'openngo-demo.herokuapp.com' }
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
