52227f0257042a375c8fbefe0e5b6a93ab1dd84b
Arwa Ahmed <arwa.tayeb@eptikar.com>
2016-03-02 11:27:23 +0300

    changed back mailer default

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-config.action_mailer.default_url_options = { host: 'openngo-demo.herokuapp.com' }
